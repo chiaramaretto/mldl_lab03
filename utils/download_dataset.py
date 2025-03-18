60815c50795505f130e789c26925e6f8a93353e2
chiaramaretto <134137190+chiaramaretto@users.noreply.github.com>
2025-03-18 16:53:08 +0100

    Update download_dataset.py

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -1,10 +1,33 @@
-dataset_path = 'http://cs231n.stanford.edu/tiny-imagenet-200.zip'  # Replace with the path to your dataset
+!wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+!unzip tiny-imagenet-200.zip -d tiny-imagenet
 
-# Send a GET request to the URL
-response = requests.get(dataset_path)
-# Check if the request was successful
-if response.status_code == 200:
-    # Open the downloaded bytes and extract them
-    with ZipFile(BytesIO(response.content)) as zip_file:
-        zip_file.extractall('/dataset')
-    print('Download and extraction complete!')
+import os
+import shutil
+
+with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+    for line in f:
+        fn, cls, *_ = line.split('\t')
+        os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+
+        shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+
+shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
+
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
+
+transform = T.Compose([
+    T.Resize((224, 224)),  # Resize to fit the input dimensions of the network
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# root/{class}/x001.jpg
+tiny_imagenet_dataset_train = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/train', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/val', transform=transform)
+
+print(f"Length of train dataset: {len(tiny_imagenet_dataset_train)}")
+print(f"Length of val dataset: {len(tiny_imagenet_dataset_val)}")
+import torch
+train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
