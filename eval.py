06deb610e376e7c81ef427e2f43b23de70fff3dd
chiaramaretto <134137190+chiaramaretto@users.noreply.github.com>
2025-03-18 17:16:34 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,4 +1,24 @@
-# Test loop
+import torch
+from torch import nn
+
+# Define the custom neural network
+class CustomNet(nn.Module):
+    def __init__(self):
+        super(CustomNet, self).__init__()
+        # Define layers of the neural network
+        # add concolutional layers to reduce the input dimension while adding more channels
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=2) # the first two parameters are the input # of channels and output # of channels
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1,stride=2)
+        self.fc1 = nn.Linear(128*56*56, 200)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x= x.flatten(start_dim=1)  # ALWAYS FLATTEN BEFORE APPLYING THE LINEAR LAYER
+        x = self.fc1(x)
+
+        return x
+        # Test loop
 def validate(model, val_loader, criterion):
     model.eval()
     val_loss = 0
